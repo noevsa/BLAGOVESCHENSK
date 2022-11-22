af4bd51db4d7e757bc3bc0ddd9b029775a677304
noevsa <noevsa@gmail.com>
2022-11-22 15:56:25 +0900

    1233124wd

--- main.py
+++ main.py
@@ -1,42 +1,15 @@
-
 from random import randint
 import sys
 from PyQt5.QtGui import QPainter, QColor, QPainterPath
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from man import Ui_MainWindow
 
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(40, 480, 75, 23))
-        self.pushButton.setObjectName("pushButton")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Кнопка"))
 class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.minSize = 300
+        self.minSize = 600
         self.maxSize = 1000
         self.pushButton.clicked.connect(self.paint)
         self.do_paint = False
@@ -54,15 +27,18 @@ class MyWidget(QMainWindow, Ui_MainWindow):
 
     def getRandomSize(self):
         return randint(10, self.minSize)
+
     def getRandomColor(self):
-        return QColor(255, 255, 204)
+        return QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+
     def draw_krug(self, qp):
         a = self.getRandomSize()
         qp.setBrush(self.getRandomColor())
         qp.drawEllipse(20, 20, a, a)
+
+
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
-
 
 
 if __name__ == '__main__':
