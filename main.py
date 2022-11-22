80e82a7e2382ea869f3091d9e75c7ca425a66e09
noevsa <noevsa@gmail.com>
2022-11-22 15:47:53 +0900

    1233124wd

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from random import randint
 import sys
 from PyQt5.QtGui import QPainter, QColor, QPainterPath
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
-import math
-from PyQt5.QtCore import Qt, QPointF
+
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('Ui.ui', self)  # Загружаем дизайн
-        self.pushButton.clicked.connect(self.run)
+        self.pushButton.clicked.connect(self.draw_krug)
         self.minSize = 300
         self.maxSize = 1000
         self.w = 0
@@ -24,12 +23,19 @@ class MyWidget(QMainWindow):
     def getRandomSize(self):
         return randint(10, self.minSize)
 
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_krug(qp)
+        qp.end()
 
     def draw_krug(self, qp):
         a = self.getRandomSize()
-        qp.setBrush(255, 255, 204)
+        qp.setBrush([255, 255, 204])
         qp.drawEllipse(100, 100, a, a)
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 
@@ -37,4 +43,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec_())
