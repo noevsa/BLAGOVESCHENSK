2d3f4c4ed4fc4f31142ccae93487911adde831c6
noevsa <noevsa@gmail.com>
2022-11-22 15:51:53 +0900

    1233124wd

--- main.py
+++ main.py
@@ -1,39 +1,65 @@
-from PyQt5 import uic
+
 from random import randint
 import sys
 from PyQt5.QtGui import QPainter, QColor, QPainterPath
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class MyWidget(QMainWindow):
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(40, 480, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Кнопка"))
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)  # Загружаем дизайн
-        self.pushButton.clicked.connect(self.draw_krug)
+        self.setupUi(self)
         self.minSize = 300
         self.maxSize = 1000
-        self.w = 0
-        self.h = 0
-        self.getRandomWinSize()
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
 
-    def getRandomWinSize(self):
-        self.w = randint(self.minSize, self.maxSize)
-        self.h = randint(self.minSize, self.maxSize)
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_krug(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
 
     def getRandomSize(self):
         return randint(10, self.minSize)
-
-    def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        self.draw_krug(qp)
-        qp.end()
-
+    def getRandomColor(self):
+        return QColor(255, 255, 204)
     def draw_krug(self, qp):
         a = self.getRandomSize()
-        qp.setBrush([255, 255, 204])
-        qp.drawEllipse(100, 100, a, a)
-
+        qp.setBrush(self.getRandomColor())
+        qp.drawEllipse(20, 20, a, a)
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
